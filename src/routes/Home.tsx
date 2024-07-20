c941dbf751d390563f84adeb8971f0e06cb76c0b
mayrang <pkss0626@naver.com>
2024-07-21 02:08:29 +0900

    challenge/slider 분리, 앞뒤 이동, title 추가

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,6 +6,7 @@ import { makeImagePath } from "../utils";
 import { AnimatePresence, motion } from "framer-motion";
 import useWindowDimensions from "../hooks/useWindowDemesion";
 import { Outlet, PathMatch, useMatch, useNavigate } from "react-router-dom";
+import Slider from "../components/Slider";
 
 const Wrapper = styled.div`
   background-color: black;
@@ -39,107 +40,13 @@ const Title = styled.h2`
   margin-bottom: 20px;
 `;
 
-const Slider = styled.div`
-  position: relative;
-  top: -100px;
-`;
-
-const Row = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  gap: 10px;
-  position: absolute;
-  width: 100%;
-`;
-
-const Box = styled(motion.div)<{ bgPhoto: string }>`
-  height: 200px;
-  color: red;
-  cursor: pointer;
-  font-size: 36px;
-  width: 100%;
-  background-image: url(${(props) => props.bgPhoto});
-  background-size: cover;
-  background-position: center center;
-  background-color: white;
-  &:first-child {
-    transform-origin: center left;
-  }
-  &:last-child {
-    transform-origin: center right;
-  }
-`;
-
-const Info = styled(motion.div)`
-  background-color: ${(props) => props.theme.black.lighter};
-  padding: 10px;
-  top: 100%;
-  transform: translateY(-100%);
-  position: relative;
-  opacity: 0;
-  bottom: 0;
-  width: 100%;
-  h4 {
-    font-size: 14px;
-
-    text-align: center;
-  }
-`;
-
-const boxVariants = {
-  normal: {
-    scale: 1,
-  },
-  hover: {
-    scale: 1.3,
-    y: -50,
-    transition: {
-      delay: 0.3,
-      type: "tween",
-    },
-  },
-};
-
-const infoVariants = {
-  normal: {
-    opacity: 0,
-  },
-  hover: {
-    opacity: 1,
-    transition: {
-      delay: 0.3,
-      type: "tween",
-    },
-  },
-};
-
-const offset = 6;
-
 export default function Home() {
   const movieIdMatch: PathMatch<string> | null = useMatch("/movie/:movieId");
-  const navigate = useNavigate();
+
   const { data, isLoading } = useQuery<IGetMovieResult>({
     queryKey: ["movies", "nowPlaying"],
     queryFn: getMovies,
   });
-  const window = useWindowDimensions();
-  const [index, setIndex] = useState(0);
-  const [leaving, setLeaving] = useState(false);
-  const onIncreaseIndex = () => {
-    if (data) {
-      if (leaving) return;
-      // 첫 영화를 빼야하므로
-      const totalLength = data.results.length - 1;
-      // 인덱스는 0부터 시작하므로
-      const maxIndex = Math.floor(totalLength / offset) - 1;
-      toggleLeave();
-      setIndex((prev) => (maxIndex === prev ? 0 : prev + 1));
-    }
-  };
-  const toggleLeave = () => setLeaving((prev) => !prev);
-  const onClickBox = (movieId: number) => {
-    navigate(`/movie/${movieId}`);
-  };
 
   console.log(data?.results.find((item) => item.id === +(movieIdMatch?.params.movieId as string)));
   return (
@@ -148,43 +55,11 @@ export default function Home() {
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner onClick={onIncreaseIndex} bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
-          <Slider>
-            <AnimatePresence initial={false} onExitComplete={toggleLeave}>
-              <Row
-                transition={{ duration: 1, type: "tween" }}
-                initial={{
-                  x: window,
-                }}
-                animate={{ x: 0 }}
-                exit={{ x: -window }}
-                key={index}
-              >
-                {data?.results
-                  .slice(1)
-                  .slice(offset * index, offset * index + offset)
-                  .map((movie) => (
-                    <Box
-                      onClick={() => onClickBox(movie.id)}
-                      variants={boxVariants}
-                      layoutId={movie.id + ""}
-                      transition={{ type: "tween" }}
-                      initial="normal"
-                      whileHover="hover"
-                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                      key={movie.id}
-                    >
-                      <Info variants={infoVariants}>
-                        <h4>{movie.title}</h4>
-                      </Info>
-                    </Box>
-                  ))}
-              </Row>
-            </AnimatePresence>
-          </Slider>
+          {data && <Slider title={"Now Playing"} data={data} />}
           <AnimatePresence>
             {movieIdMatch ? (
               <Outlet
